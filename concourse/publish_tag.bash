454cead2c16a92d2db12bb6c701eed4e7371358a
HaiSheng Yuan and Xin Zhang <xzhang@pivotal.io>
2016-05-12 17:44:34 -0700

    Add github release for GPOS [#115977749]

---
+++ concourse/publish_tag.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+set -o pipefail
+set -u -e -x
+
+main() {
+  env LC_ALL=C tar tf bin_gpos_centos5_release/bin_gpos_centos5_release.tar.gz | grep "libgpos.so." | sort -n | head -n 1 | sed 's/\.\/lib\/libgpos\.so\./v/' > gpos_github_release_stage/tag.txt
+  env GIT_DIR=gpos_src/.git git rev-parse HEAD > gpos_github_release_stage/commit.txt
+}
+
+main "$@"
